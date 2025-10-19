f38940bda6641854f94b18078fe79272c0393a13
Albino Siby <albinosiby775@gmail.com>
2025-10-20 00:15:17 +0530

    phnview

--- app.py
+++ app.py
@@ -750,4 +750,5 @@ def api_register():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run( port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
+
